76d7ebb71ef52bd2ee61669b425afd0e85002abd
Shruthi-Mp <59528137+Shruthi-Mp@users.noreply.github.com>
2020-02-08 11:30:19 +0530

    simple java file created

---
+++ simpleJavaFile.java
@@ -0,0 +1,12 @@
+public class JavaDemo
+{
+    public void display()
+    {
+        System.out.println("hello tester");
+    }
+    public static void main(String args[])
+    {
+        JavaDemo dm= new JavaDemo();
+        dm.display();
+     }
+ }
