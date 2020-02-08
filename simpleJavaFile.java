ca72902412ddc27e27d22a25472f80160a690a74
Shruthi-Mp <59528137+Shruthi-Mp@users.noreply.github.com>
2020-02-08 11:45:57 +0530

    added print method

--- simpleJavaFile.java
+++ simpleJavaFile.java
@@ -5,6 +5,11 @@ public class JavaDemo
         System.out.println("hello tester");
         System.out.println("good morning");
     }
+    public void print()
+    {
+        System.out.println("shruthi");
+        System.out.println("itpl");
+    }
     public static void main(String args[])
     {
         JavaDemo dm= new JavaDemo();
